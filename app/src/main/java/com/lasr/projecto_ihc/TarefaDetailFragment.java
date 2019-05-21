963df6501c9eecb68ac8e3a4e5b8fccf5555c099
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2019-05-21 03:13:48 +0100

    consulta activity

---
+++ app/src/main/java/com/lasr/projecto_ihc/TarefaDetailFragment.java
@@ -0,0 +1,69 @@
+package com.lasr.projecto_ihc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lasr.projecto_ihc.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Tarefa detail screen.
+ * This fragment is either contained in a {@link TarefaListActivity}
+ * in two-pane mode (on tablets) or a {@link TarefaDetailActivity}
+ * on handsets.
+ */
+public class TarefaDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TarefaDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tarefa_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.tarefa_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
