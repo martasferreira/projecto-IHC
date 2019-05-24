aabee2449a36e95cd456560f5a3e466a457a45c1
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2019-05-24 15:30:34 +0100

    dar tarefa

--- app/src/main/java/com/lasr/projecto_ihc/DarTarefa.java
+++ app/src/main/java/com/lasr/projecto_ihc/DarTarefa.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class DarTarefa extends AppCompatActivity {
 
@@ -37,6 +39,15 @@ public class DarTarefa extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Joana Silva", "Alberto Dias", "Rita Soares", "Jorge Aguiar", "Amélia Alcides"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
 
     }
 
